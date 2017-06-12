668c68a9e8c2ae87d3c6d37c61aaf569c70e1b0b
DavidsonOtobo <davidsonotobo@outlook.com>
2017-06-12 21:17:39 +0100

    Another attempt with the APIs

--- Distributor.js
+++ Distributor.js
@@ -1,6 +1,6 @@
 var stream = require('getstream');
 // Instantiate a new client (server side)
-client = stream.connect('r6h679xsbd7k', 'uu7sjcrnxp683mdfvqa8kshp4sgy8fwks2pufe2tmz9a2e2hh37vb8b5quzgv7xt', '25642');
+var client = stream.connect('r6h679xsbd7k', 'uu7sjcrnxp683mdfvqa8kshp4sgy8fwks2pufe2tmz9a2e2hh37vb8b5quzgv7xt', '25642');
 
 var user1 = client.feed('user', '1');
 
@@ -10,23 +10,19 @@ var readonlyToken = client.feed('user', '1').getReadOnlyToken();
 
 // Create a bit more complex activity
 activity = {
-	'name': 'Jack', 
-    'started_at': new Date(),
+	'actor': 'User:1', 
+    'verb': 'post',
+    'object': 1,
     'distributor_number': 07512345678,
  	'distributor_status': 'In transit',
  	'distributor_rating': 5,
     'location': {'type': 'point', 'coordinates': [37.769722,-122.476944]}
+    
 };
 
 user1.addActivity(activity)
     .then(function(data) { /* on success */ })
     .catch(function(reason) { /* on failure */ });
-
-// update the buyer status value for the activity
-activity.distributor_status = 'Delivered';
-
-// send the update to the APIs
-client.updateActivities([activity]);
 
 //client side
 // Instantiate a new client (client side)
